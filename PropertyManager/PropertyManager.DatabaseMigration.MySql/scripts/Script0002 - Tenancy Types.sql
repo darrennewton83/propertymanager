43f5cf1e8f49e5200bab80cd5fb3d04e347cf6b3
Darren Newton <darren.newton@hotmail.co.uk>
2023-03-16 10:36:25 +0000

    database migration projects

---
+++ PropertyManager/PropertyManager.DatabaseMigration.MySql/scripts/Script0002 - Tenancy Types.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE IF NOT EXISTS `propertymanager`.`tenancytype` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` NVARCHAR(250) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE);
